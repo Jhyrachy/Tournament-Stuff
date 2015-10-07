8f8bcc6cb6914c90f78458ec23d7b128d902a240
Jhyrachy <jacopopieri@gmail.com>
2015-10-07 23:44:52 +0200

    Update IscrizioniCaster2.php

--- IscrizioniCaster2.php
+++ IscrizioniCaster2.php
@@ -1,22 +1,35 @@
 <?php
 // Receive form Post data and Saving it in variables
 
-$name = @$_POST['name'];
 $nick2 = @$_POST['nick'];
 $email2 = @$_POST['email'];
 $profile = @$_POST['steamurl'];
 $url = @$_POST['sito'];
 $commento = @$_POST['commento'];
+$facebook = @$_POST['facebook'];
+$check1 = @$_POST['check1'];
 
 //sanitizzazione
 $nick = filter_var($nick2, FILTER_SANITIZE_STRING);
 $email = filter_var($email2, FILTER_SANITIZE_EMAIL);
 
 //controllo campi vuoti
+if(empty($nick)){
+		echo '<body><center>Controllare di avere inserito il Nickname!</center></body>'; //controllo del non vuoto
+		exit();
+	}
+	if(empty($email)){
+		echo '<body><center>Controllare di avere inserito la Mail!</center></body>'; //controllo del non vuoto
+		exit();
+	}
 if(empty($profile)){
 		echo '<body><center>Controllare di avere inserito il profilo Steam!</center></body>'; //controllo del non vuoto
 		exit();
 	}
+
+//controllo checkbox caster
+	$cocasting = 'No';
+if (isset($_POST['check1'])) $cocasting = 'Si';
 
 //testiamo il json
 	$filename = "casterContact.json";
@@ -53,11 +66,12 @@ $filename = "Casters.txt";
 
 // Marge all the variables with text in a single variable. 
 $f_data= '
-Nome Team: '.$name.'
 Nickname:  '.$nick.'
 Profile: '.$profile.'
 Email: '.$email.' 
+Facebook: '.$facebook.'
 Sito: '.$url.'
+Co-Casting: '.$cocasting.'
 Commento: '.$commento.'
 
 ==============================================================================
@@ -68,11 +82,12 @@ Le iscrizioni chiuderanno in data 20 Giugno. <br>
 Per favore controllate le mail (<b>Anche nello spam!</b> O aggiungete alla whitelist la mail: noreply@titadota2.com) in quei giorni per confermare la vostra presenza! <br>
 Questi sono i dati con cui vi siete registrati: <br><br>
 
-Nome Team: '.$name.'<br>
 Nickname:  '.$nick.'<br>
 Profile: '.$profile.'<br>
 Email: '.$email.'<br>
+Facebook: '.$facebook.'
 Sito: '.$url.'<br>
+Co-Casting: '.$cocasting.'
 Commento: '.$commento.'</center></body>';
 
 
@@ -88,20 +103,21 @@ $file = fopen($filename, "a");
 fwrite($file,$f_data);
 fclose($file);
 
-$to      = 'webmaster@example.com';
+$to      = 'titadota2@gmail.com';
 $subject = 'Nuovo Caster - '.$nick.'';
 $message = '
 Questi sono i dati con cui si è registrato:
 
-Nome Team: '.$name.'
 Nickname:  '.$nick.'
 Profile: '.$profile.'
-Email: '.$email.' 
+Email: '.$email.'
+Facebook: '.$facebook.'
 Sito/Stream: '.$url.'
+Co-Casting: '.$cocasting.'
 Commento: '.$commento.'
 
-Tutti gli iscritti si trovano a questo link: http://www.site.com/Casters.txt
-La mailing list si trova qui: http://www.site.com/mailingCasters.txt';
+Tutti gli iscritti si trovano a questo link: http://www.titadota2.com/phpstuff/Casters.txt
+La mailing list si trova qui: http://www.titadota2.com/phpstuff/mailingCasters.txt';
 
 $headers = 'From: newcaster@titadota2.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
@@ -118,15 +134,16 @@ Per favore controllate le mail (Anche nello spam! O aggiungete alla whitelist la
 
 Questi sono i dati con cui vi siete registrati:
 
-Nome Team: '.$name.'
 Nickname:  '.$nick.'
 Profile: '.$profile.'
-Email: '.$email.' 
+Email: '.$email.'
+Facebook: '.$facebook.'
 Sito/Stream: '.$url.'
+Co-Casting: '.$cocasting.'
 Commento: '.$commento.'';
 
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
+$headers = 'From: noreply@titadota2.com' . "\r\n" .
+    'Reply-To: postmaster@titadota2.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to2, $subject, $message, $headers);
